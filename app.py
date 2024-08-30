e077db77c688c80f4974369630aea60e91088bf7
hidawns <134594505+hidawns@users.noreply.github.com>
2024-08-30 12:28:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import joblib
 # Load the trained model and scaler
 model = joblib.load('xgboost_model.pkl')
 scaler = joblib.load('scaler (1).pkl')
-encoder = joblib.load('encoder (1).pkl')
 
 # Streamlit app
 st.title('Doctor Consultation Fee Prediction')
@@ -17,9 +16,23 @@ st.title('Doctor Consultation Fee Prediction')
 experience = st.number_input('Years of Experience', min_value=0, max_value=66, value=0)
 num_of_qualifications = st.number_input('Number of Qualifications', min_value=1, max_value=10, value=1)
 rating = st.number_input('Doctor Rating', min_value=1, max_value=100, value=1)
+
 miscellaneous_info = st.selectbox('Miscellaneous Info Existent', ['Not Present', 'Present'])
+miscellaneous_Info = 1 if miscellaneous_Info == 'Present' else 0
+
 profile = st.selectbox('Doctor Specialization', ['Ayurveda', 'Dentist', 'Dermatologist', 'ENT Specialist', 'General Medicine', 'Homeopath'])
+profile_mapping = {'Ayurveda': 0, 'Dentist': 1, 'Dermatologist': 2, 'ENT Specialist': 3, 
+                       'General Medicine': 4, 'Homeopath': 5}
+profile = profile_mapping[profile]
+
 place = st.selectbox('Place', ['Bangalore',  'Chennai', 'Coimbatore', 'Delhi', 'Ernakulam', 'Hyderabad', 'Mumbai', 'Thiruvananthapuram', 'Unknown'])
+place_mapping = {'Bangalore': 0, 'Chennai': 1, 'Coimbatore': 2, 'Delhi': 3, 'Ernakulam': 4, 
+                     'Hyderabad': 5, 'Mumbai': 6, 'Thiruvananthapuram': 7, 'Other': 8}
+place = place_mapping[place]
+
+fee_category = 0.0
+fee_category_mapping = {0.0 : 0}
+fee_category = fee_category_mapping[fee_category]
 
 # Create input DataFrame
 input_data = pd.DataFrame({
@@ -29,13 +42,8 @@ input_data = pd.DataFrame({
     'Profile': [profile],
     'Miscellaneous_Info': [miscellaneous_info],
     'Num_of_Qualifications': [num_of_qualifications],
-    'Fee_category': [0.0]  # Default value for Fee_category
+    'Fee_category': [fee_category]
 })
-
-# Map categorical variables to integer values using the encoder
-input_data['Place'] = encoder.transform(input_data['Place'])
-input_data['Profile'] = encoder.transform(input_data['Profile'])
-input_data['Miscellaneous_Info'] = encoder.transform(input_data['Miscellaneous_Info'])
 
 # Handle scaling
 input_data['Experience'] = np.sqrt(input_data['Experience'])
