c122e45d42811d2af792f94be608c68db89b3944
hidawns <134594505+hidawns@users.noreply.github.com>
2024-08-30 12:33:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ st.title('Doctor Consultation Fee Prediction')
 
 # Input fields
 experience = st.number_input('Years of Experience', min_value=0, max_value=66, value=0)
-num_of_qualifications = st.number_input('Number of Qualifications', min_value=1, max_value=10, value=1)
 rating = st.number_input('Doctor Rating', min_value=1, max_value=100, value=1)
+num_of_qualifications = st.number_input('Number of Qualifications', min_value=1, max_value=10, value=1)
 
 miscellaneous_info = st.selectbox('Miscellaneous Info Existent', ['Not Present', 'Present'])
 miscellaneous_info = 1 if miscellaneous_info == 'Present' else 0
@@ -27,7 +27,7 @@ profile = profile_mapping[profile]
 
 place = st.selectbox('Place', ['Bangalore',  'Chennai', 'Coimbatore', 'Delhi', 'Ernakulam', 'Hyderabad', 'Mumbai', 'Thiruvananthapuram', 'Unknown'])
 place_mapping = {'Bangalore': 0, 'Chennai': 1, 'Coimbatore': 2, 'Delhi': 3, 'Ernakulam': 4, 
-                     'Hyderabad': 5, 'Mumbai': 6, 'Thiruvananthapuram': 7, 'Other': 8}
+                     'Hyderabad': 5, 'Mumbai': 6, 'Thiruvananthapuram': 7, 'Unknown': 8}
 place = place_mapping[place]
 
 fee_category = 0.0
@@ -38,10 +38,10 @@ fee_category = fee_category_mapping[fee_category]
 input_data = pd.DataFrame({
     'Experience': [experience],
     'Rating': [rating],
-    'Place': [place],
-    'Profile': [profile],
-    'Miscellaneous_Info': [miscellaneous_info],
     'Num_of_Qualifications': [num_of_qualifications],
+    'Miscellaneous_Info': [miscellaneous_info],
+    'Profile': [profile],
+    'Place': [place],
     'Fee_category': [fee_category]
 })
 
